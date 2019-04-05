177ed7d34b34ecfd41c7a5ebcbe04f6cf185f6f1
ThomasBerranger <ThomasBerranger>
2019-04-05 12:29:34 +0200

    updates

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\MenuService;
 use App\Service\SlackManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     private $slackManager;
+    private $menuService;
 
-    public function __construct(SlackManager $slackManager)
+    public function __construct(SlackManager $slackManager, MenuService $menuService)
     {
         $this->slackManager = $slackManager;
+        $this->menuService = $menuService;
     }
 
     /**
@@ -20,38 +23,13 @@ class MenuController extends AbstractController
      */
     public function home()
     {
-        $chartData = [
-            'id' => 'general-history',
-            'type' => 'line',
-            'labels' => ['02/05/18', '03/05/18', '04/05/18', '05/05/18', '06/05/18', '07/05/18', '08/05/18', '09/05/18', '10/05/18', '11/05/18'],
-            'dataSets' => [
-                [
-                    'data' => [10, 20, 30, 50, 70, 60, 50, 40, 45, 50],
-                    'label' => "DDOS",
-                    'borderColor' => "#3e95cd",
-                    'fill' => 0
-                ],
-                [
-                    'data' => [20, 40, 30, 30, 35, 35, 45, 60, 70, 80],
-                    'label' => "XSS",
-                    'borderColor' => "#8e5ea2",
-                    'fill' => 0
-                ],
-            ],
-            'options' => [
-                'title' => [
-                    'display' => 0,
-                    'text' => 'A title'
-                ],
-                'responsive' => 1,
-                'legend' => [
-                    'position' => 'bottom',
-                ]
-            ],
-        ];
+        $generalAttackHistory = $this->menuService->getGeneralAttackHistory();
+
+        $generalLogHistory = $this->menuService->getGeneralLogHistory();
 
         return $this->render('menu/home.html.twig', array(
-            'chartData' => $chartData,
+            'generalAttackHistory' => $generalAttackHistory,
+            'generalLogHistory' => $generalLogHistory,
         ));
     }
 
